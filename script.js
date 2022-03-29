0bc124252d71f44f8735b15f07a30052a63d0854
Phillip48 <phillippereira02@gmail.com>
2022-03-29 14:45:07 -0400

    Added const arrays for password character set

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// Arrays for special characters, lowercase letters, uppercase letters, and numbers
+const specialChars = ["!", "@", "#", "$", "%", "^", "&", "*", "-", "=", "+", "(", ")", "{", "}", "[", "]", "?", "/", ">", "<", ".", ",", "~", "|"];
+const numberChars = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+const uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -12,3 +18,16 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+function generatePassword() {
+  var choice = 
+
+
+
+  if () {
+    
+  } else {
+    
+  }
+}
